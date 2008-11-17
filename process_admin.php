9c7074419941a2fbd4b8679e75bb7f92fbe4869e
tinezorko <tinezorko@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-17 22:11:06 +0000

    Data table uses prefix defined in config.php file of the WP installation.
    git-svn-id: https://plugins.svn.wordpress.org/tz-lock/trunk@74465 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- process_admin.php
+++ process_admin.php
@@ -8,7 +8,7 @@ $pass = get_magic_quotes_gpc() ? stripslashes($_POST['pass']) : $_POST['pass'];
 
 if ($action == "add")
 {
-  if ($GLOBALS["wpdb"]->query("INSERT INTO tzlock_plugin VALUES ('$id', PASSWORD('$pass'))") === FALSE)
+  if ($GLOBALS["wpdb"]->query("INSERT INTO " . $GLOBALS["wpdb"]->prefix . "tzlock_plugin VALUES ('$id', PASSWORD('$pass'))") === FALSE)
   {
     /*TODO: error handling (already exist or empty password)*/
   };
@@ -16,7 +16,7 @@ if ($action == "add")
 else if (substr($action, 0, 4) == "del_")
 {
   $id = substr($action, 4);
-  if ($GLOBALS["wpdb"]->query("DELETE FROM tzlock_plugin WHERE lock_id = '$id'") === FALSE)
+  if ($GLOBALS["wpdb"]->query("DELETE FROM " . $GLOBALS["wpdb"]->prefix . "tzlock_plugin WHERE lock_id = '$id'") === FALSE)
   {
     /*TODO: error handling*/  
   }
--- tzlock.php
+++ tzlock.php
@@ -3,7 +3,7 @@
 Plugin Name: TZ-Lock Plugin
 Plugin URI: http://blog.zvitek.net/index.php/tz-lock-wordpress-plugin/
 Description: This plugin will enable you to protect only desired part of your post or comment (like pictures or a video for example).
-Version: 0.4
+Version: 0.5
 Author: Tine Zorko
 Author URI: mailto:tine.zorko@gmail.com
 
@@ -24,6 +24,10 @@ Version 0.4 - Released 8.4.2008
 
 - no relative referencing in process***.php files
 
+Version 0.5 - Released 17,11,2009
+
+- WP database prefix attached to the table name
+
 */
 
 define("TZLOCK_REGEXP", "/\[lock=(\w+)\](.*?)\[\/lock\]/ims");
@@ -37,7 +41,7 @@ function tzLockReplace($match)
   $content  = $match[2];
   $pass     = $_COOKIE["tzlock_" . COOKIEHASH . "_" . $lockid];
   $GLOBALS["tzlock"]["lockid"]++; 
-  if (isset($pass) && ($dbrow = $GLOBALS["wpdb"]->get_row("SELECT * FROM tzlock_plugin WHERE lock_id='$lockid' AND lock_pass = '$pass'")));
+  if (isset($pass) && ($dbrow = $GLOBALS["wpdb"]->get_row("SELECT * FROM " . $GLOBALS["wpdb"]->prefix . "tzlock_plugin WHERE lock_id='$lockid' AND lock_pass = '$pass'")));
     if ($dbrow) return $content;
   return getUnlockForm($lockid, $GLOBALS["tzlock"]["lockid"]);
 }
@@ -49,18 +53,32 @@ function tzLockFilter($content)
 
 function activateTZLock()
 {
-  $sql = "CREATE TABLE tzlock_plugin
-  (
-    lock_id    VARCHAR(64) NOT NULL,
-    lock_pass  BINARY(41)  NOT NULL,
-    PRIMARY KEY(lock_id)
-  ) COMMENT = 'tzlock plugin'";
-  $GLOBALS["wpdb"]->query($sql);
+  $tblname = $GLOBALS["wpdb"]->prefix . "tzlock_plugin";
+  $old_tblname = "tzlock_plugin";
+  
+  $GLOBALS["wpdb"]->show_errors();
+  $GLOBALS["wpdb"]->flush();
+
+  //check if old name exists, then rename
+  if($GLOBALS["wpdb"]->get_var("SHOW TABLES LIKE '$old_tblname'"))
+  {
+    $GLOBALS["wpdb"]->query("RENAME TABLE $old_tblname TO $tblname;");
+  } else
+  {
+    $sql = "CREATE TABLE $tblname 
+    (
+      lock_id    VARCHAR(64) NOT NULL,
+      lock_pass  BINARY(41)  NOT NULL,
+      PRIMARY KEY  (lock_id)
+    ) COMMENT = 'tzlock plugin';";
+    $GLOBALS["wpdb"]->query($sql);
+  }
 }
 
 function deactivateTZLock()
 {
-  $sql = "DROP TABLE tzlock_plugin";
+  $sql = "DROP TABLE " . $GLOBALS["wpdb"]->prefix . "tzlock_plugin";
+  //do not drop ..
 }
 
 function addTZLockAdminMenu()
@@ -80,8 +98,8 @@ function displayTZLockAdminContent()
 ?>
 <div class="wrap">
   <h2>TZ-Lock Administration</h2>
-  <form name="tzlock_admin" action="<?php echo bloginfo('url') . '/' . PLUGINDIR . '/tzlock/process_admin.php';?>" method="POST">
-  <input type="hidden" name="CONFIG" value="<?php echo ABSPATH . 'wp-config.php';?>"/>
+  <form name="tzlock_admin" action="<?=bloginfo('url') . '/' . PLUGINDIR . '/tzlock/process_admin.php'?>" method="POST">
+  <input type="hidden" name="CONFIG" value="<?=ABSPATH . 'wp-config.php'?>"/>
   <table cellspacing="2" cellpadding="5" class="editform" align="center">
     <tr><td colspan="2"><h3>Novo geslo</h3></td></tr>
     <tr><td align="right">Oznaka gesla:</td><td><input type="text" name="lockid" value=""/></td></tr>
@@ -89,7 +107,7 @@ function displayTZLockAdminContent()
     <tr><td colspan="2" align="right"><button type="button" onClick="tzlock_submit('tzlock_admin', 'add')">Dodaj</button></td></tr>
 <?php
     
-  $sql = "SELECT * FROM tzlock_plugin";
+  $sql = "SELECT * FROM " . $GLOBALS["wpdb"]->prefix . "tzlock_plugin";
   $results = $GLOBALS["wpdb"]->get_results($sql);
   if ($results)
   {
@@ -102,7 +120,7 @@ function displayTZLockAdminContent()
     {
       $class = $i%2 == 0 ? " class=\"alternate\"" : "";
 ?>    
-      <tr><td width="100%"<?php echo $class;?>><?php echo $row->lock_id;?></td><td<?php echo $class;?>><button type="button" onClick="tzlock_submit('tzlock_admin', 'del_<?php echo $row->lock_id;?>')">Briši</button></td></tr>
+      <tr><td width="100%"<?=$class?>><?=$row->lock_id?></td><td<?=$class?>><button type="button" onClick="tzlock_submit('tzlock_admin', 'del_<?=$row->lock_id?>')">Briši</button></td></tr>
 <?php
       $i++;
     }
@@ -131,19 +149,19 @@ function getUnlockForm
   ob_start();
 ?>
 
-<form method="post" name="<?php echo $formName;?>" action="<?php echo bloginfo('url') . '/' . PLUGINDIR . '/tzlock/process.php';?>">
-<input type="hidden" name="CONFIG" value="<?php echo ABSPATH . 'wp-config.php';?>"/>
+<form method="post" name="<?=$formName?>" action="<?=bloginfo('url') . '/' . PLUGINDIR . '/tzlock/process.php'?>">
+<input type="hidden" name="CONFIG" value="<?=ABSPATH . 'wp-config.php'?>"/>
 <table border="0" class="tzlock_form" style="border: 1px solid #DDD" align="center">  
-  <tr><td colspan="3"><?php echo $message; ?></td></tr>
+  <tr><td colspan="3"><?=$message?></td></tr>
   <tr><td>
-    <label for="pass"><?php echo $prompt; ?></label>
+    <label for="pass"><?=$prompt?></label>
   </td><td>
     <input type="password" name="pass" value=""/>
   </td><td>
-    <a href="javascript:tzlock_submit('<?php echo $formName; ?>', 'auth')"><img src="<?php echo bloginfo('url') . '/' . PLUGINDIR . '/tzlock/unlock.gif'; ?>" border="0"/></a>
+    <a href="javascript:tzlock_submit('<?=$formName?>', 'auth')"><img src="<?=bloginfo('url') . '/' . PLUGINDIR . '/tzlock/unlock.gif'?>" border="0"/></a>
   </td></tr>
 </table>
-<input type="hidden" name="lockid" value="<?php echo $lockid; ?>"/>
+<input type="hidden" name="lockid" value="<?=$lockid?>"/>
 <input type="hidden" name="action"/>
 </form>
 
